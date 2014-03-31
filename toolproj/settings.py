96f9bb5693cd318951b6d0e6ab72ffbfb508ae53
SUGIMOTO Norimitsu <dictoss@live.jp>
2014-03-31 20:47:10 +0900

    add databases config template.

--- toolproj/settings.py
+++ toolproj/settings.py
@@ -59,6 +59,8 @@ WSGI_APPLICATION = 'toolproj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
         'NAME': 'ats',
         'USER': 'webapp',
         'PASSWORD': 'password'
--- toolproj/settings_devel.py
+++ toolproj/settings_devel.py
@@ -1,1 +1,12 @@
 APP_MOUNTDIR = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'ats',
+        'USER': 'webapp',
+        'PASSWORD': 'password'
+    }
+}
