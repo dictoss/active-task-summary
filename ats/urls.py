1a878731225fa3fe3da2829eb6310028749d1b1c
SUGIMOTO Norimitsu <dictoss@live.jp>
2017-04-09 15:28:39 +0900

    migrate url and patterns.

--- ats/urls.py
+++ ats/urls.py
@@ -1,38 +1,39 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
 
+from . import views
+
 handler500 = 'ats.views.error500'
 handler404 = 'ats.views.error404'
 
-urlpatterns = patterns('ats.views',
-         (r'^$', 'index'),
-         (r'^top/$', 'top'),
-         (r'^login/$','login_view'),
-         (r'^logout/$','logout_view'),
-         (r'^regist/$', 'regist'),
-         #
-         #(r'^summary/$', 'summary'),
-         (r'^summary/project/$', 'summary_p'),
-         (r'^summary/job/$', 'summary_j'),
-         (r'^summary/user/$', 'summary_u'),
-         # 
-         # direct link
-         #(r'^summary/(?P<project_id>\d+)/$', 'summary_p'),
-         #(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/$', 'summary_pd'),
-         #(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/u(?P<user_id>\d+)/$', 'summary_pdu'),
-         #(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/j(?P<job_id>\d+)/$', 'summary_pdj'),
-         #
-         (r'^query/$', 'query'),
-         #
-         (r'^api/$', 'api'),
-         #
-         (r'^manage/$', 'manage'),
-         #
-         (r'^manage/chpasswd/$',
-          'manage_chpasswd'),
-         )
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^top/$', views.top),
+    url(r'^login/$', views.login_view),
+    url(r'^logout/$', views.logout_view),
+    url(r'^regist/$', views.regist),
+    #
+    #url(r'^summary/$', views.summary),
+    url(r'^summary/project/$', views.summary_p),
+    url(r'^summary/job/$', views.summary_j),
+    url(r'^summary/user/$', views.summary_u),
+    #
+    # direct link
+    #url(r'^summary/(?P<project_id>\d+)/$', views.summary_p),
+    #url(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/$', views.summary_pd),
+    #url(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/u(?P<user_id>\d+)/$', views.summary_pdu),
+    #url(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/j(?P<job_id>\d+)/$', views.summary_pdj),
+    #
+    url(r'^query/$', views.query),
+    #
+    url(r'^api/$', views.api),
+    #
+    url(r'^manage/$', views.manage),
+    #
+    url(r'^manage/chpasswd/$', views.manage_chpasswd),
+]
 
 urlpatterns += staticfiles_urlpatterns()
--- toolproj/urls.py
+++ toolproj/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-         url(r'^admin/', include(admin.site.urls)),
-         url(r'^ats/', include('ats.urls')),
-         )
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^ats/', include('ats.urls')),
+]
