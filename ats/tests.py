fc9e922d99f79a7e2be7cdcd38d62b94abaa5775
SUGIMOTO Norimitsu <dictoss@live.jp>
2017-07-23 17:56:03 +0900

    add test template.

--- ats/tests.py
+++ ats/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestLib(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+class TestViews(TestCase):
+    fixtures = ['test_views.json']
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_login_top(self):
+        self.assertTrue(True)
+
+    def test_manage(self):
+        self.assertTrue(True)
+
+    def test_manage_chpasswd(self):
+        self.assertTrue(True)
+
+    def test_summary(self):
+        self.assertTrue(True)
+
+    def test_summary_project(self):
+        self.assertTrue(True)
+
+    def test_summary_job(self):
+        self.assertTrue(True)
+
+    def test_summary_user(self):
+        self.assertTrue(True)
+
+    def test_regist(self):
+        self.assertTrue(True)
