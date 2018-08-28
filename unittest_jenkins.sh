f0d11e68e68de6d9dc3e697bde9b7d199fe6a0ec
Norimitsu Sugimoto <dictoss@live.jp>
2018-08-28 21:37:18 +0900

    fix error start unittest.

--- unittest_jenkins.sh
+++ unittest_jenkins.sh
@@ -6,5 +6,8 @@ python -V
 # install libraries.
 pip install -U -r requirements.txt
 
+# create migrate file
+python manage.py makemigrations ats
+
 # execute unittest
 python manage.py test ats --settings=toolproj.settings_test
