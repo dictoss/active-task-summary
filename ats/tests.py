c4e1a7d3acee50c87a3da5c616b87f8e33090268
SUGIMOTO Norimitsu <dictoss@live.jp>
2019-01-08 23:14:23 +0900

    add test

--- ats/tests.py
+++ ats/tests.py
@@ -20,7 +20,8 @@ from .models import (
     Job,
     Task,
     Project,
-    ProjectWorker)
+    ProjectWorker,
+    UsedTaskTime)
 
 
 class AtsTestClient(Client):
@@ -85,17 +86,49 @@ class TestModel(TestCase):
         self.assertEqual(_s, '202 : projname3 [closed]')
 
     def test_projectworker(self):
-        """
-        _job = ProjectWorker.objects.create(id=100, name='jobname', sortkey=10000, invalid=False)
+        _user = User.objects.create_user(
+            'testuser1',
+            'testuser1@example.com',
+            'password')
+        _at_s = datetime.date(year=2000, month=1, day=1)
+        _proj = Project.objects.create(
+            id=200, name='projname200',
+            start_dt=_at_s, end_dt=None, sortkey=10000)
+        _job = Job.objects.create(id=100, name='job100', sortkey=10000, invalid=False)
 
-        _o = Task(id=101, name='taskname1', job=_job, sortkey=10001, invalid=False)
+        _o = ProjectWorker.objects.create(
+                id=50000, user=_user, project=_proj, job=_job, invalid=False)
         _s = str(_o)
-        self.assertEqual(_s, '101 : taskname1 (jobname)')
+        self.assertEqual(_s, '50000 : testuser1 (projname200 - job100)')
 
-        _o = Task(id=102, name='taskname2', job=_job, sortkey=10002, invalid=True)
+        _o = ProjectWorker.objects.create(
+            id=50001, user=_user, project=_proj, job=_job, invalid=True)
         _s = str(_o)
-        self.assertEqual(_s, '102 : taskname2 (jobname) [invalid]')
-        """
+        self.assertEqual(_s, '50001 : testuser1 (projname200 - job100) [invalid]')
+
+    def test_usedtasktime(self):
+        _user = User.objects.create_user(
+            'testuser1',
+            'testuser1@example.com',
+            'password')
+        _at_s = datetime.date(year=2000, month=1, day=1)
+        _proj = Project.objects.create(
+            id=200, name='proj200',
+            start_dt=_at_s, end_dt=None, sortkey=10000)
+        _job = Job.objects.create(
+            id=100, name='job100', sortkey=10000, invalid=False)
+
+        _task = Task.objects.create(id=201, name='task201',
+            job=_job, sortkey=10001, invalid=False)
+
+        _td = datetime.date(year=2019, month=2, day=1)
+        _tt = datetime.time(hour=1, minute=30, second=0)
+        _o = UsedTaskTime.objects.create(id=60000,
+            user=_user, project=_proj, task=_task,
+            taskdate=_td, tasktime=_tt)
+        _s = str(_o)
+        self.assertEqual(_s, '60000 : [2019-02-01 - 01:30:00] testuser1 - proj200 - task201')
+
 
 class TestLib(TestCase):
     def setUp(self):
