0b5e640b10543bc69bf93099b61e78714f263ea6
SUGIMOTO Norimitsu <dictoss@live.jp>
2020-05-05 10:45:50 +0900

    add field for user model on admin view.

--- ats/admin.py
+++ ats/admin.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.contrib import admin
 
 from .models import Project, Job, Task, ProjectWorker, UsedTaskTime
+
+UserAdmin.list_display = (
+    'username', 'email', 'first_name', 'last_name',
+    'is_active', 'is_superuser', 'is_staff')
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 
 class ProjectAdmin(admin.ModelAdmin):
