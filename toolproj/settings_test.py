4a7ff9e222417784228a5ae80228a74f47b7acdb
Norimitsu Sugimoto <dictoss@live.jp>
2020-12-02 23:00:13 +0900

    fix error.

--- toolproj/settings_test.py
+++ toolproj/settings_test.py
@@ -44,7 +44,6 @@ MIDDLEWARE = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
