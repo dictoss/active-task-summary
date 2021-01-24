d721d94145a971b0acabc4ed70d2346cd4f59410
Norimitsu Sugimoto <dictoss@live.jp>
2021-01-24 15:09:32 +0900

    backport master. unittest.sh use python2.7 .

---
+++ unittest.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TEST_MODE=
+BIN_PYTHON=python
+BIN_PIP=pip
+
+if [ $# -gt 0 ]; then
+    TEST_MODE=$1
+fi
+
+if [ "${TEST_MODE}" = "jenkins" ]; then
+    # check python version
+    python -V
+
+    # install libraries.
+    pip install -U -r requirements.txt
+
+    # create migrate file
+    python manage.py makemigrations ats --settings=toolproj.settings_test
+elif  [ "${TEST_MODE}" = "jenkins-docker" ]; then
+    echo "test mode is jenkins-docker, running..."
+
+    # check python version
+    ${BIN_PYTHON} -V
+
+    # install libraries.
+    ${BIN_PIP} install -U -r requirements.txt
+    ${BIN_PIP} install -U -r requirements_dev.txt
+
+    # create diretory.
+    mkdir -p ~/log
+
+    # create migrate file
+    ${BIN_PYTHON} manage.py makemigrations ats --settings=toolproj.settings_test
+fi
+
+# execute unittest
+coverage run --source='.' manage.py test -v3 ats --settings=toolproj.settings_test
+RET=$?
+echo "unittest result code: ${RET}"
+coverage report
+coverage xml
+coverage html
+exit ${RET}
