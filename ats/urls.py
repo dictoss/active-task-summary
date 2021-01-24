134ebfd81ae30ed7e21ed1eeea84d2c5c9a756c0
Norimitsu Sugimoto <dictoss@live.jp>
2021-01-24 15:41:15 +0900

    backport master. add name parameter in urls.py.

--- ats/urls.py
+++ ats/urls.py
@@ -10,16 +10,16 @@ handler500 = 'ats.views.error500'
 handler404 = 'ats.views.error404'
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^top/$', views.top),
-    url(r'^login/$', views.login_view),
-    url(r'^logout/$', views.logout_view),
-    url(r'^regist/$', views.regist),
+    url(r'^$', views.index, name='index'),
+    url(r'^top/$', views.top, name='top'),
+    url(r'^login/$', views.login_view, name='login_view'),
+    url(r'^logout/$', views.logout_view, name='logout_view'),
+    url(r'^regist/$', views.regist, name='regist'),
     #
-    #url(r'^summary/$', views.summary),
-    url(r'^summary/project/$', views.summary_p),
-    url(r'^summary/job/$', views.summary_j),
-    url(r'^summary/user/$', views.summary_u),
+    #url(r'^summary/$', views.summary, name='summary'),
+    url(r'^summary/project/$', views.summary_p, name='summary_p'),
+    url(r'^summary/job/$', views.summary_j, name='summary_j'),
+    url(r'^summary/user/$', views.summary_u, name='summary_u'),
     #
     # direct link
     #url(r'^summary/(?P<project_id>\d+)/$', views.summary_p),
@@ -27,21 +27,21 @@ urlpatterns = [
     #url(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/u(?P<user_id>\d+)/$', views.summary_pdu),
     #url(r'^summary/(?P<project_id>\d+)/(?P<from_date>\d+)_(?P<to_date>\d+)/j(?P<job_id>\d+)/$', views.summary_pdj),
     #
-    url(r'^query/$', views.query),
+    url(r'^query/$', views.query, name='query'),
     #
     #url(r'^api/$', views.api),
     #
-    url(r'^manage/$', views.manage),
+    url(r'^manage/$', views.manage, name='manage'),
     #
-    url(r'^manage/chpasswd/$', views.manage_chpasswd),
-    #url(r'^manage/user/$', views.manage_user_list),
-    #url(r'^manage/user/(?P<user_id>\d+)/$', views.manage_user_detail),
-    #url(r'^manage/project/$', views.manage_project_list),
-    #url(r'^manage/project/(?P<project_id>\d+)/$', views.manage_project_detail),
-    #url(r'^manage/task/$', views.manage_task_list),
-    #url(r'^manage/task/(?P<task_id>\d+)/$', views.manage_task_detail),
-    #url(r'^manage/job/$', views.manage_job_list),
-    #url(r'^manage/job/(?P<job_id>\d+)/$', views.manage_job_detail),
+    url(r'^manage/chpasswd/$', views.manage_chpasswd, name='manage_chpasswd'),
+    #url(r'^manage/user/$', views.manage_user_list, name='manage_user_list'),
+    #url(r'^manage/user/(?P<user_id>\d+)/$', views.manage_user_detail, name='manage_user_detail'),
+    #url(r'^manage/project/$', views.manage_project_list, name='manage_project_list'),
+    #url(r'^manage/project/(?P<project_id>\d+)/$', views.manage_project_detail, name='manage_project_detail'),
+    #url(r'^manage/task/$', views.manage_task_list, name='manage_task_list'),
+    #url(r'^manage/task/(?P<task_id>\d+)/$', views.manage_task_detail, name='manage_task_detail'),
+    #url(r'^manage/job/$', views.manage_job_list, name='manage_job_list'),
+    #url(r'^manage/job/(?P<job_id>\d+)/$', views.manage_job_detail, name='manage_job_detail'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
