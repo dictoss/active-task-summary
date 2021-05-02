d1e8a7b6f522ee1906701e0c788beb8e4b346c3f
Norimitsu Sugimoto <dictoss@live.jp>
2021-05-02 23:56:36 +0900

    backport master.:

--- toolproj/settings/production.py
+++ toolproj/settings/production.py
@@ -89,6 +89,18 @@ DATABASES = {
 
 DATABASE_ENGINE = 'postgresql'
 
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
+    },
+]
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
