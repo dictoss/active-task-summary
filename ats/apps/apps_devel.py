8f7d1ddb2488232700bab83d9aa6ee418572dc4e
Norimitsu Sugimoto <dictoss@live.jp>
2021-07-24 11:09:02 +0900

    bump up.

--- ats/apps/apps_devel.py
+++ ats/apps/apps_devel.py
@@ -12,7 +12,7 @@ class AtsConfig(AppConfig):
     verbose_name = 'Active Task Summary'
 
     # app info
-    APP_VERSION = '1.0.0'
+    APP_VERSION = '1.0.1'
     APP_AUTHER = 'dictoss'
 
     # valid range is between 1 and 24.
