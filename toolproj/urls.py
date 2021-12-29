9544ad6d85dd7cb9a1008cd2fd0a6e43f3637bd2
Norimitsu Sugimoto <dictoss@live.jp>
2021-12-29 18:49:12 +0900

    adjust old style admin screen used django-3.2.

--- toolproj/urls.py
+++ toolproj/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
-
 from django.contrib import admin
+
 admin.autodiscover()
+admin.site.enable_nav_sidebar = False
 
 handler404 = 'ats.views.error404'
 handler500 = 'ats.views.error500'
