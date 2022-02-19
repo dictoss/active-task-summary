f9027dfed8b8dc35f4c3f9755523acf780d41461
Norimitsu SUGIMOTO <dictoss@live.jp>
2022-02-19 23:30:39 +0900

    fix #49 (support AlmaLinux-8 docker images for unittest.)

--- utils/docker/docker-build.sh
+++ utils/docker/docker-build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-docker build -t dictoss/ats:debian11 - < Dockerfile.debian11
+
+ATS_DOCKER_TAG=debian11
+# ATS_DOCKER_TAG=alma8-pgsql13
+
+if [ $# -gt 0 ]; then
+    ATS_DOCKER_TAG=$1
+fi
+
+ATS_DOCKERFILE=Dockerfile.${ATS_DOCKER_TAG}
+
+docker build -t dictoss/ats:${ATS_DOCKER_TAG} - < ${ATS_DOCKERFILE}
--- utils/docker/run-unittest-in-docker.sh
+++ utils/docker/run-unittest-in-docker.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-#docker run -v ${JENKINS_HOME}/workspace/${JOB_NAME}:/mnt dictoss/ats:debian10 \
-docker run dictoss/ats:debian11 \
-    sh -c "pg_ctlcluster 13 main start && \
+ATS_PGSQL_VERSION=13
+
+ATS_DOCKER_TAG=debian11
+# ATS_DOCKER_TAG=alma8-pgsql13
+
+if [ $# -gt 0 ]; then
+    ATS_DOCKER_TAG=$1
+fi
+
+if [ $# -gt 1 ]; then
+    ATS_PGSQL_VERSION=$2
+fi
+
+if [[ "${ATS_DOCKER_TAG}" =~ "debian" ]]; then
+    ATS_PG_CTLCLUSTER_PATH="pg_ctlcluster ${ATS_PGSQL_VERSION} main "
+else
+    ATS_PG_CTLCLUSTER_PATH="sudo -u postgres /usr/pgsql-${ATS_PGSQL_VERSION}/bin/pg_ctl -D /var/lib/pgsql/${ATS_PGSQL_VERSION}/data"
+fi
+
+#docker run -v ${JENKINS_HOME}/workspace/${JOB_NAME}:/mnt dictoss/ats:${ATS_DOCKER_TAG} \
+docker run dictoss/ats:${ATS_DOCKER_TAG} \
+    sh -c "${ATS_PG_CTLCLUSTER_PATH} start && \
     git clone https://github.com/dictoss/active-task-summary.git && \
     cd active-task-summary && \
     sh unittest.sh jenkins-docker && \
